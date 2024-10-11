fb21f2189dbc40340f5d80c4ab07be7501783900
OMKARJ <omkarjoshiop@gmail.com>
2024-10-11 22:12:10 +0530

    Some changes regarding input/op

--- practice.js
+++ practice.js
@@ -1,1 +1,20 @@
 // Trying my practice problems
+const process_req = require('process');
+let getdata=" ";
+process_req.stdin.on("data",function(chunk)
+{
+    getdata+=chunk;
+    main(getdata)
+    process.exit();
+})
+
+// process_req.stdin.on("end", function()
+// {
+// main(getdata)
+// })
+function main(getdata)
+{
+    // process.stdout.write(getdata);
+    // console.log(getdata);
+    console.log(20 + getdata)
+}
